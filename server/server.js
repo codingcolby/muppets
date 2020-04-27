c02aa0620dee19042a262cda442cdb8e15580aa6
allthethings <learn2code@respectdesfonds.com>
2020-04-27 12:32:47 -0500

    added routes, imports

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+const fozzieRouter = require("./routes/fozzie.router");
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json()); // needed for angular requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("build"));
+
+/** ---------- ROUTES ---------- **/
+app.use("/wakah", fozzieRouter);
+
+/** ---------- START SERVER ---------- **/
+app.listen(port, function () {
+	console.log("Listening on port: ", port);
+});
